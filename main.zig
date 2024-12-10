16d09dfaa46e37b2e2f11458866767e7d42256aa
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-12-10 13:02:03 +0530

    Update main.zig

--- main.zig
+++ main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn split_line(allocator: std.mem.Allocator, line: []const u8) ![][]const u8 {
+pub fn tokenize_line(allocator: std.mem.Allocator, line: []const u8) ![][]const u8 {
     var list = std.ArrayList([]const u8).init(allocator);
     defer list.deinit();
     var iter = std.mem.splitScalar(u8, line, ' ');
@@ -17,13 +17,13 @@ pub fn main() !void {
     _ = args.skip();
     var input_file: [:0]const u8 = undefined;
     var output_file: [:0]const u8 = undefined;
-    if (args.next()) |ok| {
-        input_file = ok;
+    if (args.next()) |input_file_name| {
+        input_file = input_file_name;
     } else {
         @panic("Input file not specified");
     }
-    if (args.next()) |ok| {
-        output_file = ok;
+    if (args.next()) |output_file_name| {
+        output_file = output_file_name;
     } else {
         @panic("Output file not specified");
     }
@@ -38,25 +38,22 @@ pub fn main() !void {
     var resulting_html = std.ArrayList(u8).init(allocator);
     defer resulting_html.deinit();
     // Read file line by line
-    var file = try std.fs.cwd().openFile(input_file, .{});
-    defer file.close();
+    var input_file_handle = try std.fs.cwd().openFile(input_file, .{});
+    defer input_file_handle.close();
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
+    var input_file_reader = input_file_handle.reader();
     var line_count: u16 = 0;
-    var started: u16 = 0; // Count the number of tags that are starting
-    var ended: u16 = 0; // Count the number of tags that are ending
+    var started_tags: u16 = 0; // Count the number of tags that are starting
+    var ended_tags: u16 = 0; // Count the number of tags that are ending
 
-    var buf: [1024]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |input_line| {
+    var line_read_buffer: [1024]u8 = undefined;
+    while (try input_file_reader.readUntilDelimiterOrEof(&line_read_buffer, '\n')) |input_line| {
         line_count += 1;
         const line = std.mem.trim(u8, input_line, " ");
-        switch (line.len) {
-            0 => continue,
-            else => {},
-        }
-        const tokens = try split_line(allocator, line);
-        defer allocator.free(tokens);
+        if(line.len == 0) continue;
+
+        const line_as_tokens = try tokenize_line(allocator, line);
+        defer allocator.free(line_as_tokens);
 
         if (line.len == 0 or std.mem.startsWith(u8, line, "//")) {
             continue;
@@ -64,26 +61,26 @@ pub fn main() !void {
             if (!std.mem.endsWith(u8, line, "}")) {
                 @panic("Error: Did you forget to end the line {} with a '}}' ?");
             } else {
-                const completed_1 = try std.mem.replaceOwned(u8, allocator, line, "{", " ");
-                defer allocator.free(completed_1);
-                const completed_2 = try std.mem.replaceOwned(u8, allocator, completed_1, "}", " ");
-                defer allocator.free(completed_2);
-                for (completed_2) |char| {
+                const replaced_string_level_1 = try std.mem.replaceOwned(u8, allocator, line, "{", " ");
+                defer allocator.free(replaced_string_level_1);
+                const replaced_string_level_2 = try std.mem.replaceOwned(u8, allocator, replaced_string_level_1, "}", " ");
+                defer allocator.free(replaced_string_level_2);
+                for (replaced_string_level_2) |char| {
                     try resulting_html.append(char);
                 }
             }
-        } else if (tokens.len > 1 and std.mem.eql(u8, tokens[1], "end")) {
+        } else if (line_as_tokens.len > 1 and std.mem.eql(u8, line_as_tokens[1], "end")) {
             try resulting_html.append('<');
             try resulting_html.append('/');
-            for (tokens[0]) |char| {
+            for (line_as_tokens[0]) |char| {
                 try resulting_html.append(char);
             }
             try resulting_html.append('>');
             try resulting_html.append('\n');
-            ended += 1;
-        } else if (tokens.len > 0 and std.mem.eql(u8, tokens[tokens.len - 1], "end")) {
+            ended_tags += 1;
+        } else if (line_as_tokens.len > 0 and std.mem.eql(u8, line_as_tokens[line_as_tokens.len - 1], "end")) {
             try resulting_html.append('<');
-            for (tokens[0 .. tokens.len - 1]) |token| {
+            for (line_as_tokens) |token| {
                 for (token) |char| {
                     try resulting_html.append(char);
                 }
@@ -98,19 +95,20 @@ pub fn main() !void {
                 try resulting_html.append(char);
             }
             try resulting_html.append('>');
-            started += 1;
+            started_tags += 1;
         }
     }
-    var fo = try std.fs.cwd().createFile(output_file, .{});
-    defer fo.close();
-    const res = try resulting_html.toOwnedSlice();
-    defer allocator.free(res);
-    try fo.writeAll(res);
+    var output_file_handle = try std.fs.cwd().createFile(output_file, .{});
+    defer output_file_handle.close();
+    const resulting_html_as_string = try resulting_html.toOwnedSlice();
+    defer allocator.free(resulting_html_as_string);
+    try output_file_handle.writeAll(resulting_html_as_string);
 
-    if (started > ended) {
+    if (started_tags > ended_tags) {
         std.debug.print("Warning: Number of tags you created don't have their corresponding ending tags!", .{});
     }
-    if (started < ended) {
+    if (started_tags < ended_tags) {
         std.debug.print("Warning: Number of ending tags are greater than the number of starting tags.", .{});
     }
+    std.debug.print("Generated {s}.", .{output_file});
 }
